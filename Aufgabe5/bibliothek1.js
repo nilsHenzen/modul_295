3a19ca0a5bac8631db43dbf4abe1c809bb0ef359
nils <test@test.com>
2024-05-15 09:20:26 +0000

    Aufgabe 5.2

--- Aufgabe5/bibliothek1.js
+++ Aufgabe5/bibliothek1.js
@@ -33,7 +33,22 @@ let books = [
       "year": 1937,
       "author": "J.R.R. Tolkien"
     }
-]  
+]
+
+let lends = [{
+    "id": 1,
+    "customer_id": 1,
+    "isbn": 1,
+    "borrowed_at": "01.01.2024",
+    "returned_at": "02.02.2024"
+},
+{
+    "id": 2,
+    "customer_id": 2,
+    "isbn": 2,
+    "borrowed_at": "01.01.2024",
+    "returned_at": "02.02.2024"
+}]
 
 app.use(express.json());
 
@@ -84,6 +99,53 @@ app.patch('/books/:isbn', (request, response) => {
 
     response.send(books);
 
+});
+
+app.get('/lends', (request, response) => {
+    response.send(lends);
+});
+
+app.get('/lends/:id', (request, response) => {
+    const id = request.params.id;
+
+    const lend = lends.find(lend => lend.id == id);
+    response.send(lend);
+});
+
+app.post('/lends', (request, response) => {
+    const newlend = request.body;
+    const existIdArray = lends.map(lend => lend.id === newlend.id);
+    const existId = existIdArray.includes(true);
+    const booklendArray = lends.map(lend => lend.isbn === newlend.isbn);
+    const booklend = booklendArray.includes(true);
+    const userlends = lends.filter(lend => lend.customer_id === newlend.customer_id && !lend.returned_at);
+
+    if (booklend) {
+        return response.status(409).send({ message: 'This book is already lent out.' });
+    }
+
+    if (userlends.length >= 3) {
+        return response.status(409).send({ message: 'Customer already has 3 open lends.' });
+    }
+
+    if (newlend.id && newlend.customer_id && newlend.isbn && newlend.borrowed_at) {
+
+        if(!existId) {
+            lends.push(newlend);
+            response.send(newlend);
+        } else {
+            response.sendStatus(400);
+        }
+    } else {
+        response.sendStatus(422);
+    }
+});
+
+app.delete('/lends/:id', (request, response) => {
+    const id = request.params.id;
+
+    lends = lends.filter(lend => lend.id != id);    
+    response.send(lends);
 });
 
 
