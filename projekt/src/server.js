86163709d4af184d1d76cb2d2341e0626095ccf6
nils <test@test.com>
2024-05-22 09:48:11 +0000

    added swagger

--- projekt/src/server.js
+++ projekt/src/server.js
@@ -4,6 +4,11 @@ const session = require('express-session');
 const port = 3000;
 const tasks = require('./tasks');
 const authorization = require('./authorization');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
+app.use('/swagger', swaggerUi.serve);
+app.get('/swagger', swaggerUi.setup(swaggerDocument));
 
 
 app.use(express.json());
